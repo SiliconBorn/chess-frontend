ecc054f20db8490a2adca4b15a721ae176341f64
Shivam Dhaka <shivamdhaka1200@gmail.com>
2024-01-23 23:50:52 +0530

    added some styles to main game page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-// import Chat from './Components/Chat';
-// import Game from './Game';
+import Chat from './Components/Chat';
+import Game from './Game';
 // import Landing from './Components/Landing';
-import JoinRoom from './Components/JoinRoom';
+// import JoinRoom from './Components/JoinRoom';
 // import './AppVanilla.css';
 import './App.css';
 
 function App() {
 	return (
-		<div className="app min-h-full">
-			{/* <Game />
-			<Chat /> 
-			<Landing />
-	*/}
-			<JoinRoom />
+		<div className=" overflow-auto font-sans bg-slate-950 min-h-screen  px-15">
+			<div className="pt-4 px-10 mx-auto block text-center">
+				<h1 className=" text-gray-50 text-6xl font-bold uppercase my-5 drop-shadow-lg">
+					Chess
+				</h1>
+				<p className=" text-gray-50 text-lg">
+					Play with your friends online 🐴
+				</p>
+			</div>
+			<div className=" max-w-72 mx-auto mt-10 shadow-md shadow-slate-600">
+				<Game />
+			</div>
+			<Chat />
 		</div>
 	);
 }
--- src/Components/Chat.tsx
+++ src/Components/Chat.tsx
@@ -5,6 +5,9 @@ import io from 'socket.io-client';
 const useSocket = () => {
 	const [receivedMessages, setReceivedMessages] = useState<string[]>([
 		'hi there',
+		'Hello',
+		'This is the first message',
+		'This is another message from second user',
 	]);
 	const [messageInput, setMessageInput] = useState<string>('');
 	const [socket, setSocket] = useState<Socket | null>(null);
@@ -66,26 +69,28 @@ const Chat = () => {
 			<form
 				id="form"
 				action=""
-				className="chat-form"
+				className="mt-10 min-h-full text-slate-50 mx-auto max-w-fit text-center"
 				onSubmit={(e) => handleSubmit(e, socket)}
 			>
 				<div id="messages">
-					<p>Received Messages:</p>
-					<ul>
+					<p className="text-2xl font-semibold shadow-md ">CHAT</p>
+					<ul className="m-4 text-left">
 						{receivedMessages.map((msg, index) => (
 							<li key={index}>{msg}</li>
 						))}
 					</ul>
 				</div>
-				<div className="chat-box">
+				<div className="flex justify-around mx-4">
 					<input
 						id="input"
 						autoComplete="off"
-						className="chat-input"
+						className="px-2 py-1 text-slate-200 outline-none bg-slate-950 border border-slate-200 rounded-md"
 						value={messageInput}
 						onChange={(e) => setMessageInput(e.target.value)}
 					/>
-					<button className="chat-button">Send</button>
+					<button className="px-2 py-1 border text-md rounded-md cursor-pointer transition-colors text-gray-950 bg-slate-200 max-w-32">
+						Send
+					</button>
 				</div>
 			</form>
 		</>
