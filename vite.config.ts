63f5a441fafac6a2f33d2cc3e80c893dead0286e
Shivam Dhaka <shivamdhaka1200@gmail.com>
2024-01-27 11:34:15 +0530

    configured .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
@@ -12,6 +14,10 @@ export default defineConfig({
 	},
 	server: {
 		host: '0.0.0.0',
-		port: 5173,
+		port: 3000,
+	},
+
+	define: {
+		'process.env': process.env,
 	},
 });
