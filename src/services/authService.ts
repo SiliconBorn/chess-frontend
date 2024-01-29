37bb076dcdd81f1b0499e7bd42d419507fe8f3bf
Shivam Dhaka <shivamdhaka1200@gmail.com>
2024-01-29 22:33:14 +0530

    added landing bg photos

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,48 +1,48 @@
-import axios from "axios";
-import { loginUrl, registerUrl } from "../react-query/endpoints";
+import axios from 'axios';
+import { loginUrl, registerUrl } from '../react-query/endpoints';
 
 export type AuthCred = {
-  username: string;
-  password: string;
+	username: string;
+	password: string;
 };
 
 export type AuthSuccessResponse = {
-  message: string;
-  success: boolean;
-  token: string;
+	message: string;
+	success: boolean;
+	token: string;
 };
-export type AuthErrResponse = Omit<AuthSuccessResponse, "token">;
+export type AuthErrResponse = Omit<AuthSuccessResponse, 'token'>;
 
-export type RegisterResponse = Omit<AuthSuccessResponse, "token">;
+export type RegisterResponse = Omit<AuthSuccessResponse, 'token'>;
 
 const login = async ({
-  username,
-  password,
+	username,
+	password,
 }: AuthCred): Promise<AuthSuccessResponse> => {
-  try {
-    const response = await axios.post<AuthSuccessResponse>(loginUrl, {
-      username,
-      password,
-    });
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+	try {
+		const response = await axios.post<AuthSuccessResponse>(loginUrl, {
+			username,
+			password,
+		});
+		return response.data;
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
 };
 
 const register = async (registerCred: AuthCred): Promise<RegisterResponse> => {
-  try {
-    const response = await axios.post(registerUrl, registerCred);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+	try {
+		const response = await axios.post(registerUrl, registerCred);
+		return response.data;
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
 };
 
 const authService = {
-  login,
-  register,
+	login,
+	register,
 };
 export default authService;
