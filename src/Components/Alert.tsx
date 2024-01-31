54ae3a9a3ad1b4e56433277fd1239225fcf38680
Shivam Dhaka <shivamdhaka1200@gmail.com>
2024-01-31 16:27:14 +0530

    alert style done v1

--- src/Components/Alert.tsx
+++ src/Components/Alert.tsx
@@ -1,29 +1,30 @@
 const alertTypeClasses: Record<string, string> = {
 	error: 'bg-pink-700 hover:bg-pink-600 ',
 	primary: 'bg-sky-700 hover:bg-sky-600 ',
-	secondary: 'bg-slate-700 hover:bg-slate-600 ',
+	secondary: 'bg-slate-800 hover:bg-slate-700 ',
 };
 
-const iconTypeSrc: Record<string, string> = {
-	error: './cross.png',
-	success: './tick.png',
-	warning: './exclamation.png',
-};
+// const iconTypeSrc: Record<string, string> = {
+// 	error: './black_cross.png',
+// 	success: './tick.png',
+// 	warning: './exclamation.png',
+// };
 
 export default function Alert() {
 	const type: string = 'error';
-	const icon: string = 'error';
-	const msg: string = 'Uh oh, something went wrong';
+	// const icon: string = 'error';
+	const msg: string = 'Login failed';
 
 	return (
 		<div
-			className={`${alertTypeClasses[type]}opacity-90 flex items-center absolute top-4 left-1/2 md:left-auto -translate-x-1/2 md:-translate-x-0 md:top-10 md:right-10 text-white alert-error px-6 md:px-8 py-4 md:py-6 shadow-md highlight-white/20  cursor-pointer rounded-lg min-w-max`}
+			className={`${alertTypeClasses[type]}flex items-center alert md:alert  absolute top-8 left-1/2 -translate-x-1/2 md:left-auto md:-translate-x-0 md:top-10 md:right-10  text-white alert-error pl-6 pr-10 md:pl-8 md:pr-12 py-4 md:py-6 shadow-md highlight-white/20  cursor-pointer rounded-r-md rounded-tl-md min-w-max`}
 		>
 			{msg}
+
 			<img
-				src={`${iconTypeSrc[icon]}`}
-				width={25}
-				className="ml-2 md:ml-4"
+				src="./cross.png"
+				className="absolute right-4 rounded-full  scale-100 hover:scale-110"
+				width={15}
 			/>
 		</div>
 	);
